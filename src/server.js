4c5bffb913527a4985e1810e588b6eef38942ae4
Kevin Lopes <kevinlopes2304@gmail.com>
2025-01-27 14:25:15 -0300

    Rotas de criação e listagem

--- src/server.js
+++ src/server.js
@@ -1,7 +1,35 @@
 import http from 'http'
 
+
+const users = []
+
+
+
+
+
+// Criando um server HTTP usando o createServer do módulo http  
 const server = http.createServer((req, res) =>{
-    return res.end('Hello World')
+    const { method, url } = req
+
+    if(method === 'GET' && url == '/users'){
+      return res
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users));
+    }
+
+
+    if(method == 'POST' && url =='/users'){
+        users.push({
+            id: 1,
+            name:'John Doe',
+            email:'johndoe@example.com'
+        })
+
+        return res.writeHead(201).end()
+    }
+
+    
+    return res.writeHead(404).end()
 })
 
 server.listen(3333)
