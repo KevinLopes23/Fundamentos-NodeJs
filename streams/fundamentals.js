b2a7ef833d29b722b16c9fabaecd490ccb2442cc
Kevin Lopes <kevinlopes2304@gmail.com>
2025-01-28 22:46:53 -0300

    Aplicado streams

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,27 +1,40 @@
-process.stdin
-    .pipe(process.stdout)
+process.stdin.pipe(process.stdout);
 
+import { Readable, Writable, Transform } from "node:stream";
+import { buffer } from "node:stream/consumers";
 
-    import {Readable} from 'node:stream'
-import { setTimeout } from 'node:timers/promises'
+class OneToHungredStream extends Readable {
+  index = 1;
+  _read() {
+    const i = this.index++;
 
-    class OneToHungredStream extends Readable{
-        index = 1
-      _read(){
-        const i = this.index++
-        
-        setTimeout(() => {
-            if(i > 100){
-            this.push(null)
-        }else {
-            const buf = Buffer.from(String(i))
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null);
+      } else {
+        const buf = Buffer.from(String(i));
 
-            this.push(buf)
-        }
-    }, 1000)
-        
-      }  
-    }
+        this.push(buf);
+      }
+    }, 1000);
+  }
+}
 
-    new OneToHungredStream()
-    .pipe(process.stdout)
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
+
+    callback(null, Buffer.from(String(transformed)));
+  }
+}
+
+class MultiplyByTenStrem extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback();
+  }
+}
+
+new OneToHungredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStrem());
